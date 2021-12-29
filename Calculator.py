42a19ffb0e18ad6f5fe678e3809f01fc6f7352be
bil10 <66562471+bil10@users.noreply.github.com>
2021-12-29 15:30:17 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+a = int(input('Enter the first number'))
+b = int(input('Enter the second number'))
+
+sum = a+b
+subtract = a-b
+multiply = a*b
+divide = a/b
+ 
+if sum =
+print(sum)
+print(subtract)
+print(multiply)
+print(divide)
+
